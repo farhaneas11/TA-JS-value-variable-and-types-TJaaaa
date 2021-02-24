caeb70388df8d923cf50be65ba4faf72f50af833
farhaneas11 <farhan.salam11@gmail.com>
2021-02-24 12:40:09 +0530

    js

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -4,10 +4,21 @@
 - Define a variable named `username` using `const` keyword and assign your name as a value.
 - Define another variable named `bio` and assign a value `Learning to code`. Use `let` keyword to define the variable.
 */
+/*
+const username=`farhan`;
+let bio = `learning to code`;
+alert (username + "is" + bio);
+message = `I am ${username} and my bio is ${bio}`;
+alert(message)
+*/
 
+
+/*
 let firstName = "Arya";
 let lastName = "Stark";
-
+message=`I am ${firstName} ${lastName} of winterfell`
+alert(message);
+*/
 /* Do the following using `+` operator
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
@@ -20,6 +31,10 @@ let lastName = "Stark";
 
 let amount = 200;
 const taxPercent = 10;
+message = `Total amount is ${amount}`
+
+message1 =`the tax amount is ${amount* taxPercent/100}`;
+alert(message +"\n" +message1);
 
 /* Use string template literal:
   - Alert a message saying `The total amount is [amount]
